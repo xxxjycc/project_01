f3154956a9f3e22087344d22436d21d057bd2910
itheima <itheima@itcast.cn>
2022-10-08 23:23:36 +0800

    ‘提交’

---
+++ Desktop/aaaa/toutiao-m/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <h1>黑马头条</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+<style lang="less">
+
+</style>
